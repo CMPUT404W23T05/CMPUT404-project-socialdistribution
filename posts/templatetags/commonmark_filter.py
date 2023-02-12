28f8111d8d9f21268c063098e298c432be83a091
geofferb <38046772+geofferb@users.noreply.github.com>
2023-02-12 14:27:38 -0700

    #11 Convert function to django template filter

--- posts/templatetags/commonmark_filter.py
+++ posts/templatetags/commonmark_filter.py
@@ -1,14 +1,20 @@
 # markdown_it usage at https://markdown-it-py.readthedocs.io/en/latest/using.html
 # CommonMark reference: https://commonmark.org/help/
+from django import template
+from django.template.defaultfilters import stringfilter
 from markdown_it import MarkdownIt
+register = template.Library()
 
 
-def markToHTML(input):
+@register.filter()
+@stringfilter
+def commonMark(value):
     '''Given a string in CommonMark, return it in HTML form.'''
-    return MarkdownIt('commonmark').render(input)
+    return MarkdownIt('commonmark').render(value)
 
 
 if __name__ == "__main__":
+    # a basic test
     testString = '# hello!'
     # print(testString)
-    assert (markToHTML(testString) == "<h1>hello!</h1>\n")
+    assert (commonMark(testString) == "<h1>hello!</h1>\n")
