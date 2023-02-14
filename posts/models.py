8bca5e5f5eef084885ef7d2d35b05879e1cbe527
John Macdonald <macjohnny56@gmail.com>
2023-02-14 16:17:44 -0700

    added basic implemtation of making public posts

--- posts/models.py
+++ posts/models.py
@@ -7,6 +7,7 @@ SMALLER_MAX_LENGTH = 50
 
 
 class Post(models.Model):
+    # "type": "post"
     object_type = models.CharField(max_length=SMALLER_MAX_LENGTH)
     title =  models.CharField(max_length=MAX_LENGTH) # title of a post
     post_id = models.URLField(max_length=MAX_LENGTH) # id of a post
@@ -15,8 +16,8 @@ class Post(models.Model):
     description = models.TextField(max_length=MAX_LENGTH) # a brief description of the post
     content_type = models.CharField(max_length=SMALLER_MAX_LENGTH)
     content = models.TextField(max_length=MAX_LENGTH)
-  #  author = models.ForeignKey(Author, on_delete=models.CASCADE) # an author can write many posts
-    # put in categories here
+    # author = models.ForeignKey(Author, on_delete=models.CASCADE) # an author can write many posts
+    # put in categories here: a list of string
     comment_count = models.IntegerField()
     comments = models.URLField(max_length=MAX_LENGTH) 
     # commentsSrc is OPTIONAL and can be missing
--- posts/views.py
+++ posts/views.py
@@ -1,21 +1,46 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound
 from .models import Post
+import datetime
 
 
-# def index(request):
-#     return render(request, 'posts/index.html')
-
+'''
+default view for posts page
+'''
 def index(request):
     if request.method == 'POST':
-            # if request.content_type == 'application/json':
         object_type = request.POST.get('type')
         if object_type == 'post':
+            if request.content_type == 'text/plain':
+                plain_text_post(request)
             message = "Post creation successful!"
         else:
             message = "Post creation failed."
-            title = request.POST.get('title')
         return render(request, 'posts/index.html', {'message': message})
     else:
         return render(request, 'posts/index.html')
 
+'''
+make a public post
+'''
+def plain_text_post(request):
+    post = Post()
+    post.title = request.POST.get('title')
+    post.post_id = "http://127.0.0.1:5454/authors/2/posts/2" # need to actually generate a new id for post eventually
+
+    # these are placeholdes for now, because I don't know how to deal with these yet
+    post.post_source = "https://www.example.com/source"
+    post.post_origin = "https://www.example.com/origin"
+    post.description = "example description"
+    post.comment_count = 0
+    post.comment = "http://127.0.0.1:5454/authors/2/posts/2/comments"
+
+    post.content_type = request.POST.get('contentType')
+    post.content = request.POST.get('content')
+    post.pub_date = datetime.datetime.utcnow().isoformat()  
+    post.is_unlisted = False     # only true for image posts
+    post.visibility = "PUBLIC"   # this is a public post, need to figure out how to pass whether its public or not
+    post.save()
+
+
+    
