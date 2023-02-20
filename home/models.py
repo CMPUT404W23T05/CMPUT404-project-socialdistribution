13d833a390950e2ddcc9baee477ab545199988f4
John Macdonald <macjohnny56@gmail.com>
2023-02-20 12:12:24 -0700

    intergrated authors app into home app

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 MAX_LENGTH = 100
 SMALLER_MAX_LENGTH = 50
@@ -73,3 +74,39 @@ class Comment(models.Model):
     pub_date = models.DateTimeField()
     comment_id = models.URLField(max_length=MAX_LENGTH)
     # comment = models.ForeignKey(Comments, on_delete=models.CASCADE) (brings up an error when running server)
+
+
+'''
+{
+"type": "authors",
+"items": [ {"type":"author",...}, ...]
+}
+'''
+class Authors(models.Model):
+    object_type = models.CharField(max_length=SMALLER_MAX_LENGTH)
+
+
+
+'''
+{
+"type": "author",
+"id": ...
+"host": ...
+"displayName": ...
+"url": ...
+"github": ...
+"profileImage": ...
+}
+'''
+class Author(models.Model):
+    object_type = models.CharField(max_length=SMALLER_MAX_LENGTH)
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # ID of the author
+    home_host = models.URLField() # the home host
+    display_name = models.CharField(max_length=MAX_LENGTH) # the display name
+    profile_url = models.URLField(max_length=MAX_LENGTH) # url to the author's profile
+    author_github = models.URLField(max_length=MAX_LENGTH) # HATEOS url for Github API
+    profile_image = models.URLField(max_length=MAX_LENGTH) # Image from a public domain (or ImageField?)
+
+    def __str__(self):
+        # clearer description of object itself rather than Author(1) in admin interface
+        return self.display_name
--- home/tests.py
+++ home/tests.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
-from .models import Post
+from .models import Post, Author
 from datetime import datetime
 from django.urls import reverse
-from .models import Post
 
-class ModelTesting(TestCase):
+
+class PostTesting(TestCase):
 
     def setUp(self):
         self.post = Post.objects.create(
@@ -89,3 +89,23 @@ class PlainTextPostTesting(TestCase):
 
 
     # def test_missing_title(self):
+
+class AuthorTesting(TestCase):
+
+    def setUp(self):
+        self.author = Author.objects.create(
+                object_type = "author",
+                home_host = "http://127.0.0.1:5454/",
+                display_name = "Gandalf the Grey",
+                profile_url = "http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+                author_github = "http://github.com/gandalfthegrey",
+                profile_image = "https://i.imgur.com/k7XVwpB.jpeg"
+                )
+
+    def test_author_model_is_valid(self):
+        d = self.author
+        self.assertTrue(isinstance(d, Author))
+
+    def test_author_model_str(self):
+        d = self.author
+        self.assertEqual(str(d), "Gandalf the Grey")
