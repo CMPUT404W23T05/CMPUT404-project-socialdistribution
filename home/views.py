438e86b488c5a116b1afa3126b84ec1a81d0b646
John Macdonald <macjohnny56@gmail.com>
2023-03-07 18:53:38 -0700

    yolo v2

--- home/views.py
+++ home/views.py
@@ -29,7 +29,7 @@ class CreatePost(APIView):
 
 class PostList(APIView, PageNumberPagination):
     def get(self, request, author_id, format=None):
-        posts = Post.objects.filter(visibility='PUBLIC', author__id=author_id)
+        posts = Post.objects.filter(visibility='PUBLIC', author__uid=author_id)
 
         self.page = request.query_params.get('page', 1)
         self.page_size = request.query_params.get('size', 20)
