ba53415f561825785d466854f9621fc4603773dc
toppingart <toppingart123@gmail.com>
2023-03-16 17:36:28 -0600

    moving social views to separate file

--- home/social_views.py
+++ home/social_views.py
@@ -31,85 +31,6 @@ from django.forms.models import model_to_dict
 from django.core.serializers.json import DjangoJSONEncoder
 
 
-
-class CreatePost(APIView):
-    # @permission_classes([IsAuthenticated])
-    def post(self, request, author_id, format=None):
-        try:
-            Author.objects.get(uid=author_id)
-        except Author.DoesNotExist:
-            raise Http404 
-
-        serializer = PostDeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class BrowsePosts(APIView, PageNumberPagination):
-    def get(self, request, format=None):
-        posts = Post.objects.filter(visibility='PUBLIC')
-
-        self.page = int(request.query_params.get('page', 1))
-        self.page_size = int(request.query_params.get('size', 20))
-
-        results = self.paginate_queryset(posts, request, view=self)
-        serializer = PostSerializer(results, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class PostList(APIView, PageNumberPagination):
-    def get(self, request, author_id, format=None):
-        posts = Post.objects.filter(visibility='PUBLIC', author__uid=author_id)
-
-        self.page = int(request.query_params.get('page', 1))
-        self.page_size = int(request.query_params.get('size', 20))
-
-        results = self.paginate_queryset(posts, request, view=self)
-        serializer = PostSerializer(results, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)        
-
-
-class PostDetail(APIView):
-    def get_object(self, post_id):
-        try:
-            return Post.objects.get(post_id=post_id)
-        except Post.DoesNotExist:
-            raise Http404 
-
-        # FOR RETRIEVING THE DETAILS OF A GIVEN POST
-    def get(self, request, post_id, author_id, format=None):
-        post = self.get_object(post_id)
-        serializer = PostSerializer(post)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-        # PUT DOES NOT WORK CURRENTLY - for creating a post from another node in db
-    def put(self, request, post_id, author_id, format=None):
-        post = self.get_object(post_id)
-        serializer = PostDeSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        # FOR EDITING EXISTING POST
-    @permission_classes([IsAuthenticated])
-    def post(self, request, post_id, author_id, format=None):
-        post = self.get_object(post_id)
-        serializer = PostDeSerializer(instance=post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        # FOR DELETING EXISITING POST
-    @permission_classes([IsAuthenticated])
-    def delete(self, request, post_id, author_id, format=None):
-        post = self.get_object(post_id)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
 class FollowersList(APIView):
     '''
     URL: ://service/authors/{AUTHOR_ID}/followers
@@ -233,15 +154,6 @@ class FollowingList(APIView):
         except Author.DoesNotExist:
             raise Http404 
         
-    def check_author_in_followers(self, current_author_info):
-        '''
-        Checks if the current author is following anyone
-        '''
-        try:
-            return Author.objects.filter(followers_items__author_info = current_author_info)[0]
-        except: # the author does not exist
-            raise Http404
-        
     def get(self, request, author_id):
         '''
         Get a list of authors that the current author is following (extra - not in spec, local)
@@ -257,8 +169,8 @@ class FollowingList(APIView):
         current_author_info = json_info.decode("utf-8")
 
         # check if the author is the followers_list of the authors
-        following_authors = self.check_author_in_followers(current_author_info)
-
+        following_authors = Author.objects.filter(followers_items__author_info = current_author_info)
+ 
         # returns a list of Author objects that the current author is following
         serializer = AuthorSerializer(following_authors, many=True)
         return Response(serializer.data)
@@ -272,15 +184,6 @@ class FriendsList(APIView):
             return Author.objects.get(uid=author_id)
         except Author.DoesNotExist:
             raise Http404 
-
-    def check_author_in_followers(self, current_author_info):
-        '''
-        Checks if the current author is following anyone
-        '''
-        try:
-            return Author.objects.filter(followers_items__author_info = current_author_info)[0]
-        except: # the author does not exist
-            raise Http404
         
     def get(self, request, author_id):
         '''
@@ -298,7 +201,7 @@ class FriendsList(APIView):
 
         # check who the current author is following, and who is following
         # the current author
-        following_list = self.check_author_in_followers(current_author_info)
+        following_list = Author.objects.filter(followers_items__author_info = current_author_info)
         follower_list = current_author.followers_items.all() # who is following them
 
         list_of_friends = []  
@@ -355,142 +258,4 @@ class RequestsDetails(APIView):
             raise Http404
         else:
             follow.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class ImageView(APIView):
-    def get(self, request, author_id, post_id, format=None):
-        post = Post.objects.get(post_id=post_id)
-        image, content_type = post.get_image()
-
-        if not image:
-            raise Http404
-
-        return HttpResponse(image, content_type=content_type)
-
-
-
-class AuthorList(APIView, PageNumberPagination):
-    def get(self, request, format=None):
-        authors = Author.objects.all()
-
-        self.page = int(request.query_params.get('page', 1))
-        self.page_size = int(request.query_params.get('size', 20))
-
-        results = self.paginate_queryset(authors, request, view=self)
-        serializer = AuthorSerializer(results, many=True)
-        response = {
-                "type": "authors",
-                "items": serializer.data
-                }
-        return Response(response, status=status.HTTP_200_OK)
-
-class AuthorDetail(APIView):
-    def get_object(self, author_id):
-        try:
-            return Author.objects.get(uid=author_id)
-        except Author.DoesNotExist:
-            raise Http404 
-
-    def get(self, request, author_id, format=None):
-        author = self.get_object(author_id)
-        serializer = AuthorSerializer(author)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request, author_id, format=None):
-        author = self.get_object(author_id)
-        serializer = AuthorSerializer(instance=author, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_200_OK)
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-class CommentList(APIView, PageNumberPagination):
-    def get(self, request, post_id, author_id, format=None):
-        comments = Comment.objects.filter(post_id=post_id)
-
-        self.page = int(request.query_params.get('page', 1))
-        self.page_size = int(request.query_params.get('size', 5))
-
-        results = self.paginate_queryset(comments, request, view=self)
-        serializer = CommentSerializer(results, many=True)
-        response = {
-                "type": "comments",
-                "page": self.page.number,
-                "size": self.page_size,
-                "post": request.build_absolute_uri().rstrip('/comments'),
-                "id": request.build_absolute_uri(),
-                "comments": serializer.data
-                }
-
-        return Response(response, status=status.HTTP_200_OK)
-
-    # @permission_classes([IsAuthenticated])
-    def post(self, request, post_id, author_id, format=None):
-        serializer = CommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class CommentDetail(APIView):
-    def get_object(self, comment_id):
-        try:
-            return Comment.objects.get(comment_id=comment_id)
-        except Comment.DoesNotExist:
-            raise Http404 
-
-    def get(self, request, post_id, author_id, comment_id, format=None):
-        comment = self.get_object(comment_id)
-        serializer = CommentSerializer(comment)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class InboxDetails(APIView, PageNumberPagination):
-    """
-    Get the inbox of the author
-    """
-    def get(self, request, author_id):
-        try:
-            # get the current author and set up its id url
-            current_author = Author.objects.filter(uid=author_id)[0]
-            inbox = AuthorInboxSerializer(current_author)
-            print(inbox.data)
-
-           # self.page = int(request.query_params.get('page',1))
-           # self.page_size = int(request.query_parms.get('size',20))
-
-        except:
-            raise Http404
-        else:
-            # returns a list of the follows
-            return Response(inbox.data, status=status.HTTP_200_OK)   
-             
-    def post(self, request, author_id):
-
-        try:
-            # get the current author and set up its id url
-            current_author = Author.objects.filter(uid=author_id)[0]
-            inbox = AuthorInboxSerializer(current_author)
-        except:
-            raise Http404
-        else:
-            # returns a list of the follows
-            current_author.inbox_items.create(inbox_item = request.data)
-            return Response(status=status.HTTP_201_CREATED)
-    
-    def delete(self, request, author_id):
-        try:
-            # get the current author and set up its id url
-            current_author = Author.objects.filter(uid=author_id)[0]
-            inbox = AuthorInboxSerializer(current_author)
-        except:
-            raise Http404
-        else:
-            # returns a list of the follows
-            all_items = current_author.inbox_items.all()
-            all_items.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
