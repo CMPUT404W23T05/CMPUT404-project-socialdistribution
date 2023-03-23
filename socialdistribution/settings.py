f610b7ed58eaba6d45337b68d065ca5e4f0fc59e
geofferb <38046772+geofferb@users.noreply.github.com>
2023-03-22 18:50:03 -0600

    #44: Migrate to postgres

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -104,8 +104,14 @@ WSGI_APPLICATION = 'socialdistribution.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+                'options': '-c search_path=myschema'
+            },
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'somepassword',
+        'HOST': 'localhost'
     }
 }
 
