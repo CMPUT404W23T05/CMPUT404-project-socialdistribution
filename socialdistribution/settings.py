f23649ed3067ec5cfbc5e186bdf2e1208a75c0c0
geofferb <38046772+geofferb@users.noreply.github.com>
2023-03-22 19:20:18 -0600

    #44: use gunicorn for heroku deploy

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -158,3 +159,4 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_on_heroku.settings(locals())
