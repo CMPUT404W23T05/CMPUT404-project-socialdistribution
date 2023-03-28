af865b8a0c24d337899096a4d68364cec2bfcf01
John Macdonald <macjohnny56@gmail.com>
2023-03-28 13:38:35 -0600

    removed django schema

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -125,7 +125,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'OPTIONS': {
-            'options': '-c search_path=django,public'
+            'options': '-c search_path=public'
         },
         'NAME': 'mydatabase',
         'USER': 'myuser',
