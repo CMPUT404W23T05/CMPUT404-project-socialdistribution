dbe8b748396a1b7ce08d5daa5c256bcace954abc
FunnyBot9980 <77359830+FunnyBot9980@users.noreply.github.com>
2023-04-01 17:55:06 -0600

    added second, generic exception statement in PostList

--- home/views.py
+++ home/views.py
@@ -107,6 +107,8 @@ class PostList(APIView, PageNumberPagination):
                     posts = Post.objects.filter(author__author_id=author_id)
         except AttributeError:
             posts = Post.objects.filter(visibility='PUBLIC', author__author_id=author_id)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
         self.page = int(request.query_params.get('page', 1))
