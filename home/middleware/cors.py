ae8ec80508bc957839342dabc8c47db988de8a22
FunnyBot9980 <77359830+FunnyBot9980@users.noreply.github.com>
2023-04-07 11:44:53 -0600

    add allow methods header

--- home/middleware/cors.py
+++ home/middleware/cors.py
@@ -6,4 +6,5 @@ from django.utils.deprecation import MiddlewareMixin
 class CorsMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         response["Access-Control-Allow-Origin"] = "*"
+        response['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
         return response
