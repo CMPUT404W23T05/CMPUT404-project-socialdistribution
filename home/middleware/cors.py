1242801653bd5fbb3279170782e2ea4bfcad0ce4
John Macdonald <macjohnny56@gmail.com>
2023-04-07 11:08:25 -0600

    added custom cors middleware class

---
+++ home/middleware/cors.py
@@ -0,0 +1,9 @@
+ # -*- coding:utf-8 -*-
+
+ from django.http import HttpResponse
+ from django.utils.deprecation import MiddlewareMixin
+
+ class CorsMiddleware(MiddlewareMixin):
+     def process_response(self, request, response):
+         response["Acces-Control-Allow-Origin"] = "*"
+         return response
